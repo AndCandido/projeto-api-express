3cdc5914365becbef579ab3b68164aa24ba148fe
André <100149329+AndCandido@users.noreply.github.com>
2023-03-24 20:06:15 -0300

    Update tasksMiddlewares.js

--- backend/src/middlewares/tasksMiddlewares.js
+++ backend/src/middlewares/tasksMiddlewares.js
@@ -1,4 +1,5 @@
 exports.validateBody = (req, res, next) => {
-    if(!req.body.title) return res.status(404)
+    if(!req.body.title) 
+        return res.status(400).json({ message: "'title' is required"})
     next()
-}
+} 
