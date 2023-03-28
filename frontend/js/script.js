569db199a3406b39a7db692765728b00525c7e20
André <100149329+AndCandido@users.noreply.github.com>
2023-03-28 17:43:17 -0300

    utilizando a rota put com upadates

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,6 +3,7 @@ const addForm = document.querySelector('.add-form')
 const inputTask = document.querySelector('.input-task')
 const url = 'http://localhost:3003/tasks'
 
+
 const fetchTasks = async () => {
 
     const response = await fetch(url)
@@ -14,23 +15,35 @@ const addTask = async (e) => {
     e.preventDefault()
     const body = { title: inputTask.value }
 
-    const fetchOptions = {
+    const options = {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
     }
-    await fetch(url, fetchOptions)
+    await fetch(url, options)
     loadTasks()
     clearInput()
 }
 
 const deleteTask = async (id) => {
-    console.log(id)
+    const urlWithId = url + '/' + id
+    const options = { method: 'delete' }
+    await fetch(urlWithId, options)
+    loadTasks()
 }
 
-const editTask = (e) => {
+const updateTask = async ({id, title, status }) => {
+    const urlWithId = url + '/' + id
+    const options = {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({title, status})
+    }
 
+    await fetch(urlWithId, options)
+    loadTasks()
 }
+
 
 const formateDate = (dateUTC) => {
     const options = {
@@ -81,24 +94,29 @@ const createBtnAction = (type, taskId) => {
             button.addEventListener('click', () => deleteTask(taskId))
             break
         case 'edit':
-            button.addEventListener('click', () => editTask(taskId))
+            button.addEventListener('click', () => updateTask(taskId))
             break
     }
     return button
 }
 
 const createRow = (task) => {
+    const { id, title, created_at, status } = task
     const tableRow = createElement('tr')
 
-    const tdTitle = createElement('td', task.title)
-    const tdCreatedAt = createElement('td', formateDate(task.created_at))
+    const tdTitle = createElement('td', title)
+    const tdCreatedAt = createElement('td', formateDate(created_at))
     const tdStatus = createElement('td')
     const tdBtnAction = createElement('td')
 
-    tdStatus.appendChild(createSelect())
+    const select = createSelect()
+    select.addEventListener('change', 
+        ({ target }) => updateTask({ ...task, status: target.value }))
 
-    tdBtnAction.appendChild(createBtnAction('edit', task.id))
-    tdBtnAction.appendChild(createBtnAction('delete', task.id))
+    tdStatus.appendChild()
+
+    tdBtnAction.appendChild(createBtnAction('edit', id))
+    tdBtnAction.appendChild(createBtnAction('delete', id))
 
     tableRow.appendChild(tdTitle)
     tableRow.appendChild(tdCreatedAt)
